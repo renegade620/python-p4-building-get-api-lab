2296f96764d9ff41db4ac1a9f7ea3bc1116966ba
Frankline Were <franklinegift@gmail.com>
2024-10-07 23:13:48 +0300

    cuild get api

--- server/app.py
+++ server/app.py
@@ -20,19 +20,26 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    return make_response(  bakeries,   200  )
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id=id).first()
+    bakery_serialized = bakery.to_dict()
+    return make_response ( bakery_serialized, 200  )
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods= BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_serialized = [baked_good.to_dict() for baked_good in baked_goods]
+    return make_response(baked_goods_serialized, 200)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    baked_good_serialized = baked_good.to_dict()
+    return make_response(baked_good_serialized, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
